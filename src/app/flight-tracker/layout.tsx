45f3423f9e84bb20a74a4d935a707f95786c92c4
Paul Catalan <24451518+polcats@users.noreply.github.com>
2024-08-04 19:20:22 +0800

    chore: added favicon; updated keywords

--- src/app/flight-tracker/layout.tsx
+++ src/app/flight-tracker/layout.tsx
@@ -2,6 +2,31 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Flight Tracker',
+  description: 'Track flights in real-time',
+  keywords: [
+    'Flight Tracker',
+    'OpenSky Network',
+    'Aircraft Tracking',
+    'Aircraft',
+    'Airplane',
+    'Air Traffic',
+    'Air Traffic Control',
+    'Air Traffic Management',
+    'Air Traffic Monitoring',
+    'Air Traffic Surveillance',
+    'Air Traffic Tracking',
+    'Air Traffic Visualization',
+    'Flight',
+    'Flight Tracking',
+    'Flight Visualization',
+    'OpenSky',
+    'OpenSky API',
+    'OpenSky Network API',
+    'Real-time Flight Tracking',
+    'Real-time Flight Visualization',
+    'Real-time Tracking',
+    'Real-time Visualization',
+  ],
 };
 
 export default function RootLayout({
